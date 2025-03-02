65a4923054e9e4e005fddc4682df281399d21efb
Theta312 <114621804+Theta312@users.noreply.github.com>
2025-03-02 15:40:07 -0500

    adding type unknown and cofigured next config ts

--- src/app/utils/test-utils.tsx
+++ src/app/utils/test-utils.tsx
@@ -18,7 +18,7 @@ interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
       ...renderOptions
     }: ExtendedRenderOptions = {}
   ) {
-    function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+    function Wrapper({ children }: PropsWithChildren<unknown>): JSX.Element {
       return <Provider store={store}>{children}</Provider>
     }
     return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
